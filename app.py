eba4fcc4783e683711685781f06793afb3c94834
st25112-a11y <st25112@ormiston.school.nz>
2026-05-27 11:08:37 +1200

    Added code that retrieves stored data

--- app.py
+++ app.py
@@ -7,5 +7,20 @@ from flask import Flask, render_template, request, session, flash, redirect, url
 app = Flask(__name__)
 app.secret_key = 'key'
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+def load_data():
+    try:
+        with open('data/services.json') as f:
+            data = json.load(f)
+        with open('data/services.json') as f:
+            services = json.load(f)
+        return data, services
+    except(FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error loading data: {e}")
+        return {}, {}
+
 if __name__ == '__main__':
     app.run(debug=True)
