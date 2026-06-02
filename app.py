9b3ca088a48659438cb7e679dc9a10c01059885b
st25112-a11y <st25112@ormiston.school.nz>
2026-06-03 11:02:41 +1200

    Added more html files for different pages + added basic coding for template

--- app.py
+++ app.py
@@ -8,8 +8,28 @@ app = Flask(__name__)
 app.secret_key = 'key'
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/order_history')
+def order_history():
+    return render_template('order_history.html')
+
+@app.route('/ordering_menu')
+def ordering_menu():
+    return render_template('ordering_menu.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
 
 def load_data():
     try:
@@ -21,6 +41,7 @@ def load_data():
     except(FileNotFoundError, json.JSONDecodeError) as e:
         print(f"Error loading data: {e}")
         return {}, {}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
